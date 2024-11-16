46c728463d0ae81599691db9a34b6215243cfe74
Emmanuel M. Oliveira | OFS <166438565+oliveirafullstack@users.noreply.github.com>
2024-11-16 19:50:00 -0300

    Development (#1)
    * chore: developed the initial structure and style guide
    
    * chore: Development of the Finalized Project

---
+++ tailwind.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        back: "##F5F4F0",
+        borderPrimary: "#D9D9D9",
+        textPrimary: "#1B1B1B",
+        textSecondary: "#333",
+        textTertiary: "#696969",
+        textHeadline: "#E1624F",
+        acentBlue: "#0C51A7",
+        acentOrange: "#E95E10",
+        acentPink: "#E5245E",
+        acentPurple: "#591B98",
+      },
+      fontFamily: {
+        sans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
